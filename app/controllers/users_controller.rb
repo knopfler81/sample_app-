ee89a1bcac9b76bfc0cf6a7b45e066aaa9a4a944
Nelly Duclos <knopfler42@yahoo.fr>
2017-11-20 22:39:59 +0100

    finished sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,31 @@
 class UsersController < ApplicationController
 
   attr_accessor :name, :email
+
   def new
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+      flash[:sucess] = "Welcome to the Sample App!"
+    else
+      render :new
+    end
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
