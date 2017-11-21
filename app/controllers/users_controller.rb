ed6cc8a255cbf5460453fb9840efb8adccac6b26
Nelly Duclos <knopfler42@yahoo.fr>
2017-11-21 21:35:28 +0100

    Basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,28 @@
 class UsersController < ApplicationController
 
-  attr_accessor :name, :email
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
   end
 
-
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      flash[:sucess] = "Welcome to the Sample App!"
     else
       render :new
     end
   end
 
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
