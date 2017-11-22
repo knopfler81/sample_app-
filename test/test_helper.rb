a63e6c56b76e1773c4ad191b21be04ea97d6a56b
Nelly Duclos <knopfler42@yahoo.fr>
2017-11-22 22:51:09 +0100

    fixed session controller create

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,18 +7,20 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  # Add more helper methods to be used by all tests here...
   include ApplicationHelper
 
+  # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-
+  # Log in as a particular user.
   def log_in_as(user)
     session[:user_id] = user.id
   end
 end
-
 
 class ActionDispatch::IntegrationTest
 
@@ -29,3 +31,4 @@ class ActionDispatch::IntegrationTest
                                           remember_me: remember_me } }
   end
 end
+
